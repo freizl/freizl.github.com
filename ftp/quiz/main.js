3a87434a0accf746eb7376ef0d207bffb4e81d70
Haisheng.Wu <freizl@gmail.com>
2015-05-23 17:06:20 -0700

    a few new.

---
+++ ftp/quiz/main.js
@@ -0,0 +1,45 @@
+// === 1
+
+function F(){};
+var f = new F();
+console.log(f.__proto__ === F.prototype);
+F.prototype.someProperty = "some value";
+console.log(f.someProperty);
+console.log(f instanceof F);
+
+F.prototype = {anotherProperty: "another value"};
+
+console.log(f.someProperty);
+console.log(f.anotherProperty);
+console.log(f instanceof F);
+
+// === 2
+function a(x) {
+    return x * 2;
+}
+var a;
+console.log(a);
+
+// === 3
+
+var x = new String("abc"),
+    y = String("abc"),
+    z = "" + "abc";
+console.log(x == y)
+console.log(x == z)
+console.log(y == z)
+console.log(x === y);
+console.log(x === z);
+console.log(y === z);
+
+// === 4
+// http://www.ecma-international.org/ecma-262/5.1/#sec-11.9.3
+console.log('' == '0'); // false
+console.log(0 == '');   // true
+console.log(0 == '0');  // true
+console.log(0 == false);  // true
+console.log(false == 'false'); // false
+console.log(false == '0'); // true
+console.log(false == undefined); // false
+console.log(false == null); // false
+console.log(null == undefined); // true
